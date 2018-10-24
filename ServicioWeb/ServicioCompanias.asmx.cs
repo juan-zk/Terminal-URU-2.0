c6c6b78cea3c7d693abebe62a95cb863958931eb
ziryon <36709361+ziryon@users.noreply.github.com>
2018-10-24 01:00:18 -0300

    esqueleto armado
    quite las referencias y puse los servicios web

---
+++ ServicioWeb/ServicioCompanias.asmx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace ServicioWeb
+{
+    /// <summary>
+    /// Descripción breve de ServicioCompanias
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Para permitir que se llame a este servicio Web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
+    // [System.Web.Script.Services.ScriptService]
+    public class ServicioCompanias : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+    }
+}
---
+++ ServicioWeb/ServicioViajes.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="ServicioViajes.asmx.cs" Class="ServicioWeb.ServicioViajes" %>
