c20a830d45f10befbfe22458b4701e5d9891cd2b
fermanzi15 <41448270+fermanzi15@users.noreply.github.com>
2018-11-11 13:58:36 -0300

    fer-Empleado
    ABMEmpleado

---
+++ Administracion/ABMEmpleado.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace Administracion
+{
+    partial class ABMEmpleado
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNombreCompleto = new System.Windows.Forms.TextBox();
+            this.txtContraseña = new System.Windows.Forms.TextBox();
+            this.txtCedula = new System.Windows.Forms.TextBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.lblError = new System.Windows.Forms.Label();
+            this.EPNCedula = new System.Windows.Forms.ErrorProvider(this.components);
+            this.BarraHerramientas = new System.Windows.Forms.ToolStrip();
+            ((System.ComponentModel.ISupportInitialize)(this.EPNCedula)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ingrese Cedula:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ingrese Contraseña:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 170);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(132, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Ingrese Nombre Completo:";
+            // 
+            // txtNombreCompleto
+            // 
+            this.txtNombreCompleto.Location = new System.Drawing.Point(194, 170);
+            this.txtNombreCompleto.Name = "txtNombreCompleto";
+            this.txtNombreCompleto.Size = new System.Drawing.Size(209, 20);
+            this.txtNombreCompleto.TabIndex = 3;
+            // 
+            // txtContraseña
+            // 
+            this.txtContraseña.Location = new System.Drawing.Point(194, 100);
+            this.txtContraseña.Name = "txtContraseña";
+            this.txtContraseña.Size = new System.Drawing.Size(209, 20);
+            this.txtContraseña.TabIndex = 4;
+            // 
+            // txtCedula
+            // 
+            this.txtCedula.Location = new System.Drawing.Point(194, 50);
+            this.txtCedula.Name = "txtCedula";
+            this.txtCedula.Size = new System.Drawing.Size(209, 20);
+            this.txtCedula.TabIndex = 5;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(136, 247);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 6;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.Location = new System.Drawing.Point(245, 247);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(75, 23);
+            this.btnModificar.TabIndex = 7;
+            this.btnModificar.Text = "Modificar";
+            this.btnModificar.UseVisualStyleBackColor = true;
+            this.btnModificar.Click += new System.EventHandler(this.btnModificar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(354, 246);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.TabIndex = 8;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // lblError
+            // 
+            this.lblError.AutoSize = true;
+            this.lblError.Location = new System.Drawing.Point(245, 316);
+            this.lblError.Name = "lblError";
+            this.lblError.Size = new System.Drawing.Size(39, 13);
+            this.lblError.TabIndex = 9;
+            this.lblError.Text = "lblError";
+            // 
+            // EPNCedula
+            // 
+            this.EPNCedula.ContainerControl = this;
+            // 
+            // BarraHerramientas
+            // 
+            this.BarraHerramientas.Location = new System.Drawing.Point(0, 0);
+            this.BarraHerramientas.Name = "BarraHerramientas";
+            this.BarraHerramientas.Size = new System.Drawing.Size(837, 25);
+            this.BarraHerramientas.TabIndex = 10;
+            this.BarraHerramientas.Text = "toolStrip1";
+            // 
+            // ABMEmpleado
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(837, 440);
+            this.Controls.Add(this.BarraHerramientas);
+            this.Controls.Add(this.lblError);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnModificar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.txtCedula);
+            this.Controls.Add(this.txtContraseña);
+            this.Controls.Add(this.txtNombreCompleto);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "ABMEmpleado";
+            this.Text = "ABMEmpleado";
+            ((System.ComponentModel.ISupportInitialize)(this.EPNCedula)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNombreCompleto;
+        private System.Windows.Forms.TextBox txtContraseña;
+        private System.Windows.Forms.TextBox txtCedula;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Label lblError;
+        private System.Windows.Forms.ErrorProvider EPNCedula;
+        private System.Windows.Forms.ToolStrip BarraHerramientas;
+    }
+}
---
+++ Administracion/ABMEmpleado.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using Administracion.ServicioWeb;
+
+namespace Administracion
+{
+    public partial class ABMEmpleado : Form
+    {
+
+        private Empleado _Emp = null;
+        
+        public ABMEmpleado()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void DesactivoBotones()
+        {
+            btnAgregar.Enabled = false;
+            btnEliminar.Enabled = false;
+            btnModificar.Enabled = false;
+        }
+
+        private void LimpioCajaTexto()
+        {
+            txtCedula.Text = "";
+            txtContraseña.Text = "";
+            txtNombreCompleto.Text = "";
+            lblError.Text = "";
+        }
+
+        private void txtCedula_Validating(object sender, CancelEventArgs e)
+        {
+            try
+            {
+                Convert.ToInt32(txtCedula.Text);
+                EPNCedula.Clear();
+            }
+            catch (Exception ex)
+            {
+                EPNCedula.SetError(txtCedula, "Solo se pueden ingresar numeros");
+                e.Cancel = true;
+            }
+
+            try
+            {
+                _Emp = new Administracion.ServicioWeb.ServicioTURU().BuscarEmpleado(txtCedula.Text);
+                this.LimpioCajaTexto();
+
+                if (_Emp == null)
+                {
+                    btnAgregar.Enabled = true;
+                }
+                else
+                {
+                    btnModificar.Enabled = true;
+                    btnEliminar.Enabled = true;
+                    txtCedula.Text = _Emp._Cedula;
+                    txtContraseña.Text = _Emp._Contraseña;
+                    txtNombreCompleto.Text = _Emp._NombreCompleto;
+                }
+            }
+            catch (System.Web.Services.Protocols.SoapException ex)
+            {
+                
+            }
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                _Emp._Cedula = txtCedula.Text.Trim();
+                _Emp._Contraseña = txtContraseña.Text.Trim();
+                _Emp._NombreCompleto = txtNombreCompleto.Text.Trim();
+
+                new Administracion.ServicioWeb.ServicioTURU().AgregarEmpleado(_Emp);
+                this.DesactivoBotones();
+                this.LimpioCajaTexto();
+
+                lblError.Text = "Alta con Exito";
+            }
+            catch (System.Web.Services.Protocols.SoapException ex)
+            { }
+            catch (Exception ex)
+            { }
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                new Administracion.ServicioWeb.ServicioTURU().EliminarEmpleado(_Emp);
+                this.DesactivoBotones();
+                this.LimpioCajaTexto();
+                lblError.Text = "Baja con exito";
+            }
+            catch (System.Web.Services.Protocols.SoapException ex)
+            { }
+            catch (Exception ex)
+            { }
+        }
+
+        private void btnModificar_Click(object sender, EventArgs e)
+        {
+            try 
+            {
+                _Emp._Cedula = txtCedula.Text.Trim();
+                _Emp._Contraseña = txtContraseña.Text.Trim();
+                _Emp._NombreCompleto = txtNombreCompleto.Text.Trim();
+                new Administracion.ServicioWeb.ServicioTURU().ModificarEmpleado(_Emp);
+
+                this.DesactivoBotones();
+                this.LimpioCajaTexto();
+                lblError.Text = " Modificar con Exito";
+            }
+            catch (System.Web.Services.Protocols.SoapException ex)
+            { }
+            catch (Exception ex)
+            { }
+        }
+
+
+
+        
+    }
+}
